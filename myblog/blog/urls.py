a96600917f3cb59f657e8202ff42f384698dd3d3
Maksym Khanin <st01237589@stud.duikt.edu.ua>
2025-11-21 08:28:14 +0200

    Add URL pattern for post detail view by date and slug

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -6,4 +6,8 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('blog/<int:id>/', views.post_detail, name='post_detail'),
+    # Маршрут для перегляду поста за датою та slug
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_detail,
+         name='post_detail'),
 ]
