c117432ff4133f5404c0d6d3e446d5d07dbb1ec1
maksymkhanin <buxhacker@gmail.com>
2025-11-21 08:47:00 +0200

    Refactor post list view to use ListView and update pagination template variable

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,6 +1,13 @@
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_list(request):
     post_list = Post.published.all()
