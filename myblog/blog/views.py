d07c12e36746dbd4c5aa7a7f7f0a239a5cf6385a
Maksym Khanin <st01237589@stud.duikt.edu.ua>
2025-11-21 08:28:19 +0200

    Add detailed post view by date and slug

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -8,3 +8,13 @@ def post_list(request):
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+
+    return render(request, 'blog/post/detail.html', {'post': post})
