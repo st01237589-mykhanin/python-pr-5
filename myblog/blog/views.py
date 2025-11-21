e60bc1669d3133e7f791589f01e0e987b0cd2443
maksymkhanin <buxhacker@gmail.com>
2025-11-21 08:45:39 +0200

    Refactor pagination logic in post_list view to handle exceptions for page numbers

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,14 +1,18 @@
 from .models import Post
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
     post_list = Post.published.all()
-    # Пагінація з 3 постами на сторінку
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
-    
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, year, month, day, post):
