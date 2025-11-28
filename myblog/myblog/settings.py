5c57fbe1c34f8504161e8f9a5382eb095a272286
maksymkhanin <buxhacker@gmail.com>
2025-11-28 08:46:38 +0200

    Update database settings to use PostgreSQL instead of SQLite

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': 'xxxxxx',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
