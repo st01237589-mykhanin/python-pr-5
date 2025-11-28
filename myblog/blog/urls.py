5497caf8be56f54d734b76f8fd959ae33c4003bc
maksymkhanin <buxhacker@gmail.com>
2025-11-28 08:14:24 +0200

    Add URL pattern for tag-based post listing

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
          name='post_detail'),
     path('post/<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
 ]
