9ed5051d165b8eeb0c2a14056b744924111d67c2
maksymkhanin <buxhacker@gmail.com>
2025-11-28 08:40:26 +0200

    Fix feed URL path and add SITE_ID to settings

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-    path('feed/', LatestPostsFeed(), name='post_feed'),
+    path('blog/feed/', LatestPostsFeed(), name='post_feed'),
 ]
--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sitemaps',
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
