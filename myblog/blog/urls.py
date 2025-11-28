47b9b1993bfd619f93abb3f63400aec0779e8a93
maksymkhanin <buxhacker@gmail.com>
2025-11-28 08:29:58 +0200

    Add feed

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -3,6 +3,8 @@ from . import views
 from django.contrib.sitemaps import Sitemap
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
+from .feeds import LatestPostsFeed
+
 sitemaps = {
     'posts': PostSitemap,
 }
@@ -20,4 +22,5 @@ urlpatterns = [
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
